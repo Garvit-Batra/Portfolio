ba1b2a9687fed03fc296a96968c1a594c5ae7d01
Garvit Batra <batra.garvit28@gmail.com>
2023-03-16 01:42:33 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Link, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import React from "react";
 import Home from "./components/Home";
 import About from "./components/About";
